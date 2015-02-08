4691e452dbee6bcc9cfd0a896e54cc21a223a52b
George Field <gf12815@my.bristol.ac.uk>
2015-02-08 19:19:58 +0000

    Add a second test building spawn to the SE corner. Use this to fix building scaling and centre positioning.

--- Source/GPProject/GPGameMode.cpp
+++ Source/GPProject/GPGameMode.cpp
@@ -27,8 +27,11 @@ AGPGameMode::AGPGameMode(const class FObjectInitializer& ObjectInitializer)
 void AGPGameMode::StartPlay()
 {
     Super::StartPlay();
-	//SpawnBuilding(FVector(-230.0f, 100.0f, 300.0f), FVector(2.0f,2.0f,10.0f));
+
+	//Should spawn a building at the NW and SE corners of the map...
 	SpawnBuilding(FVector2D(0.0f, 0.0f), FVector2D(64.0f, 64.0f));
+	SpawnBuilding(FVector2D(576.0f, 576.0f), FVector2D(640.0f, 640.0f));
+
     StartMatch();
 
     if (GEngine)
@@ -39,26 +42,47 @@ void AGPGameMode::StartPlay()
 
 void AGPGameMode::SpawnBuilding(FVector2D const a, FVector2D const b)
 {
+	// World size TODO: Calculate this!
+	const float worldx = 1000.0f;
+	const float worldy = 1000.0f;
+
+	// World NW corner offset (The origin is currently at the centre of the floor object) TODO: Move to 0,0?
+	const FVector worldOffset = FVector(-1.0f * (worldx / 2.0f), -1.0f * (worldy / 2.0f), 0.0f);
+
+	// Building mesh size TODO: Calculate this!
+	const float meshx = 200.0f;
+	const float meshy = 200.0f;
+
+	// Dimensions of the building rectangle (in pixels!)
+	const float sqx_px = FMath::Max(a.X, b.X) - FMath::Min(a.X, b.X);
+	const float sqy_px = FMath::Max(a.Y, b.Y) - FMath::Min(a.Y, b.Y);
+
+	// Dimensions of the entire input space (in pixels!)
+	const float worldx_px = 640.0f;
+	const float worldy_px = 640.0f; // TODO: Switch this for 480 and resize the world so it matches the aspect ratio.
+
+	// Get the unscaled centre point.
 	FVector2D centre2D = (a + b) / 2.0f;
 	FVector centre = FVector(centre2D, 0.0f);
-	FRotator rot = centre.Rotation();
 
+	FRotator rot = centre.Rotation();
 	// Offset the rotation by 45 as we got this from the diagonal.
 	rot.Yaw -= 45.0f;
 
-	// World size TODO: Calculate this!
-	float worldx = 1000.0f;
-	float worldy = 1000.0f;
-	float meshx = 105.0f;
-	float meshy = 105.0f;
+	// Scale factors for mesh scaling.
+	const float scalex = (sqx_px * worldx) / (worldx_px * meshx);
+	const float scaley = (sqy_px * worldy) / (worldy_px * meshy);
 
-	float sqx_px = FMath::Max(a.X, b.X) - FMath::Min(a.X, b.X);
-	float sqy_px = FMath::Max(a.Y, b.Y) - FMath::Min(a.Y, b.Y);
-	float worldx_px = 640.0f;
-	float worldy_px = 640.0f; // TODO: Switch this for 480 and resize the world so it matches the aspect ratio.
+	// Scale factors for points in the world.
+	const float cscalex = worldx / worldx_px;
+	const float cscaley = worldy / worldy_px;
 
-	float scalex = (sqx_px * worldx) / (worldx_px * meshx);
-	float scaley = (sqy_px * worldy) / (worldy_px * meshy);
+	// Scale the centre points up into game coordinates.
+	centre.X *= cscalex;
+	centre.Y *= cscaley;
+
+	// Offset the centre for differing input vs game origins
+	centre += worldOffset;
 
 	return SpawnBuilding(centre, rot, FVector(scalex, scaley, 5.0f));
 }
